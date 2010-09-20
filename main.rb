91c0b4286241134fc764cb567cbb3ef7cce58b2e
Yan <rottled@gmail.com>
2010-09-20 17:15:39 -0400

    Initial commit

---
+++ main.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+
+if ARGV.size == 0
+	$stderr.write("Usage: #{$0} file.msbin\n")
+	exit 1
+end
+
+def read_byte(handle)
+	return handle.read(1)[0]
+end
+
+def read_long(handle)
+	return handle.read(4).unpack('L')
+end
+
+def read_string(handle)
+	return handle.read(read_long(handle))
+end
+
+def read_record(handle)
+	record_type = read_byte(handle)
+	print "record type: #{record_type}"
+end
+
+def read_msbin(handle)
+	while !handle.eof()
+		read_record(handle)
+	end
+end
+
+f = File.new(ARGV[0], "rb")
+read_msbin(f)
+puts f
